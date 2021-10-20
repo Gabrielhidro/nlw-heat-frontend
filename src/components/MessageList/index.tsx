9f40ef72e636e07f05c7b203c92797a5bd0053b3
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-10-19 22:23:43 -0300

    Get api github datas

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,41 +1,48 @@
 import styles from './style.module.scss'
 import logoImg from '../../assets/logo.svg'
+import { api } from '../../services/api'
+import { useEffect, useState } from 'react'
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
 
 export function MessageList(){
+
+  const [ messages, setMessages ] = useState<Message[]>([])
+
+    useEffect(() => {
+      api.get<Message[]>('messages/last3')
+        .then(response => {
+          setMessages(response.data);
+        })
+    }, [])
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="Logo" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio culpa numquam omnis ut in ea deserunt libero mollitia quidem laborum natus, qui nemo incidunt voluptatibus cumque laudantium aspernatur nobis odit!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/gabrielhidro.png" alt="user image" />
-            </div>
-            <span>Gabriel Freitas</span>
-          </div>
-        </li>
+        {messages.map(message => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          )
+        })}
 
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio culpa numquam omnis ut in ea deserunt libero mollitia quidem laborum natus, qui nemo incidunt voluptatibus cumque laudantium aspernatur nobis odit!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/gabrielhidro.png" alt="user image" />
-            </div>
-            <span>Gabriel Freitas</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio culpa numquam omnis ut in ea deserunt libero mollitia quidem laborum natus, qui nemo incidunt voluptatibus cumque laudantium aspernatur nobis odit!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/gabrielhidro.png" alt="user image" />
-            </div>
-            <span>Gabriel Freitas</span>
-          </div>
-        </li>
+        
       </ul>
     </div>
   )
